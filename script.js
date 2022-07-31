0c8313e0ca8d172e279834e9f230b0cb2de44b9c
Adanna Emenike <89314904+Adaemenike@users.noreply.github.com>
2022-07-31 17:48:21 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log('name')
+let goat = 'adanna';
+
+// I will come to this
+let goatCap = goat.toUpperCase();
+let z = goat.slice(1, 6);
+
+let firstLetter = goat[0].toUpperCase();
+let result = firstLetter + z;
+console.log(result);
+
+let country = '   Nigeria  ';
+console.log(country.trim());
+
+let height = '    173cm  ';
+console.log(height.trim());
